92433a254e75b1358d245e217b11b4da2b950131
Kaizersatou <91896506+Kaizersatou@users.noreply.github.com>
2021-10-04 21:33:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi người tôi yêu",
+    introTitle: 'Huyền-chan',
+    introDesc: `Em là người quan trọng nhất đối với anh,
+    không ai có thể thay thế đc em <333
+    thế nên là yêu anh nhá
+    anh hứa là sẽ quan tâm em,chiều em hết sức luôn á <33333`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,9 +12,9 @@ const CONFIG = {
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'yêu <3',
+    mess: 'Bạn biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'thương lắm luôn đó.',
+    btnAccept: 'iu mà <33333',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
